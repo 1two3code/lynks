2dc4abcd1c094346bff975335762ddea0ee6e41d
Hasse <mail@1two3code.com>
2023-03-27 23:36:14 +0200

    Added some --demo stuff to ink

--- cli/source/cli.ts
+++ cli/source/cli.ts
@@ -7,18 +7,23 @@ export const args = meow(
 
 	Options
 		--name  Your name
+		--demo
 
 	Examples
 	  $ cli --name=Jane
 	  Hello, Jane
 `,
-	{
+	{	
 		flags: {
 			name: {
+				alias: "n",
 				type: "string",
 			},
+			demo: {
+				type: "boolean"
+			}
 		},
-	}
+	},
 );
 export type ArgsType = typeof args;
 export type FlagsType = typeof args.flags;
--- cli/source/ui.tsx
+++ cli/source/ui.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Text } from "ink";
 import { FlagsType } from "./cli";
+import Demo from "./demo";
 
 type AppProps = {
-	flags?: FlagsType
+	flags?: Partial<FlagsType>;
 };
 export default ({ flags }: AppProps) => (
-	<Text>
-		Hello, <Text color="green">{flags?.name}</Text>
-	</Text>
+	<>
+		{flags?.demo ? (
+			<Demo />
+		) : (
+			<Text>
+				Hello, <Text color="green">{flags?.name}</Text>
+			</Text>
+		)}
+	</>
 );
